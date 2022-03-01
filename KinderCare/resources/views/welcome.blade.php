4d0da16b73c2170d9354cdbfcbcd748f94722955
LwangaConrad <lwangaconradarthur@gmail.com>
2022-03-01 18:48:32 +0300

    what was presented

--- KinderCare/resources/views/welcome.blade.php
+++ KinderCare/resources/views/welcome.blade.php
@@ -17,6 +17,7 @@
         <style>
             body {
                 font-family: 'Nunito', sans-serif;
+                background-image: url('images/back.jpg');
             }
         </style>
     </head>
@@ -68,12 +69,6 @@
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-
-                <div class="flex justify-center mt-4 sm:items-center sm:justify-between">
-                    <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
-                        Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
                     </div>
                 </div>
             </div>
